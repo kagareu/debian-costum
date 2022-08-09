8725cff92dfef06d02667c342f2d6963cf12a4a5
kagareu <41589203+kagareu@users.noreply.github.com>
2022-08-09 23:29:01 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ apt update
 sudo apt install unzip picom bspwm polybar sddm dunst qterminal thunar flameshot neofetch sxhkd git lxappearance xorg curl -y
 
 # install Albert
-echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
+sudo echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+sudo curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
 sudo apt update
 sudo apt install albert
