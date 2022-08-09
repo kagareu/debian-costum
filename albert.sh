e2ae904d064470a7556bd9ae63686b2d4da9b6be
kagareu <41589203+kagareu@users.noreply.github.com>
2022-08-09 23:17:32 +0100

    Create albert.sh

---
+++ albert.sh
@@ -0,0 +1,5 @@
+# install Albert
+echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
+sudo apt update
+sudo apt install albert
